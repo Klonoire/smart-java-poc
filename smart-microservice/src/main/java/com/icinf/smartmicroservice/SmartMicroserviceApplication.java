37d483c1397bb813478d1ec3a44579543a05a5d4
Sebastian Reyes <sireyes@ubiobio.cl>
2022-12-28 19:12:53 -0300

    Revert "eureka"
    This reverts commit f4d053c2a1d86d846e1c4a6f60f4f1c80bf87c43.

--- smart-microservice/src/main/java/com/icinf/smartmicroservice/SmartMicroserviceApplication.java
+++ smart-microservice/src/main/java/com/icinf/smartmicroservice/SmartMicroserviceApplication.java
@@ -2,18 +2,14 @@ package com.icinf.smartmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
 
 @EnableFeignClients
 @SpringBootApplication
-@EnableDiscoveryClient
 public class SmartMicroserviceApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SmartMicroserviceApplication.class, args);
 	}
-
 
 }
