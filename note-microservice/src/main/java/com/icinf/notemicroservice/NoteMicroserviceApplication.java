9d5234e5df4eb9b39abaca9cdcbfc65440765df5
Sebastian Reyes <sireyes@ubiobio.cl>
2022-12-28 21:34:43 -0300

    Eureka Service

--- note-microservice/src/main/java/com/icinf/notemicroservice/NoteMicroserviceApplication.java
+++ note-microservice/src/main/java/com/icinf/notemicroservice/NoteMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.icinf.notemicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NoteMicroserviceApplication {
 
 	public static void main(String[] args) {
